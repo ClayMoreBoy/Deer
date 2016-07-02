cf8e8c365ae8c5f6b8bb72612dca9047040d8c63
Deerweak <17009654234@qq.com>
2016-07-02 12:27:54 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * 鹿乃 - LVLVL.CN 原创主题，整体采用兼容思想设计，自适应各种终端形式。
+ * 鹿弱 - LVLVL.CN  Deer 主题，整体采用兼容思想设计，自适应各种终端形式。
  * 
- * @package LuNai
+ * @package Deer
  * @author 陆公子
  * @version 1.9.2
  * @link http://lvlvl.cn
